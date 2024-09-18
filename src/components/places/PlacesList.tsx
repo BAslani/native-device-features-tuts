c2be383491604b1324c91e405ae1811f512ee024
behzad aslani <behzadaslani80@gmail.com>
2024-09-18 14:11:51 +0330

    feat: places list boilerplates

--- src/components/places/PlacesList.tsx
+++ src/components/places/PlacesList.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 
-export default function PlacesList() {
+import { Place } from '~/types/types'
+
+type Props = {
+  places: Place[]
+}
+
+export default function PlacesList({ places }: Props) {
   return (
-    <View>
-      <Text>PlacesList</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      )}
+    />
   )
 }
