6894d0d6489e1e9f6305ce7831a5095407209417
behzad aslani <behzadaslani80@gmail.com>
2024-09-19 13:33:53 +0330

    feat: started working on the place form

--- src/components/places/PlaceForm.tsx
+++ src/components/places/PlaceForm.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
 
 export default function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState('')
+
+  const changeTitleHandler = (text: string) => {
+    setEnteredTitle(text)
+  }
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView className='flex-1 p-6'>
+      <View>
+        <Text className='font-bold mb-1 text-primary700'>Title</Text>
+        <TextInput
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+          className='my-2 px-1 py-2 text-base border-b-primary700 border-b-2 bg-primary100'
+        />
+      </View>
+    </ScrollView>
   )
 }
