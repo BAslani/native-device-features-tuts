f895d66edda4cd7fea67f6acf8327feb5c06d444
behzad aslani <behzadaslani80@gmail.com>
2024-10-04 15:32:29 +0330

    feat: implemented reverse geocoding for address

--- src/components/places/PlaceForm.tsx
+++ src/components/places/PlaceForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { View, Text, ScrollView } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 
@@ -7,13 +7,19 @@ import LocationPicker from './LocationPicker'
 import Button from '../UI/Button'
 
 type Props = {
-  createPlaceHandler: (title: string, imageUri: string, location: { latitude: number; longitude: number }) => void
+  createPlaceHandler: (
+    title: string,
+    imageUri: string,
+    address: string,
+    location: { latitude: number; longitude: number }
+  ) => void
 }
 
 export default function PlaceForm({ createPlaceHandler }: Props) {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [pickedImage, setPickedImage] = useState<string | null>(null)
   const [pickedLocation, setPickedLocation] = useState<{ latitude: number; longitude: number } | null>(null)
+  const [address, setAddress] = useState<string | null>(null)
 
   const changeTitleHandler = (text: string) => {
     setEnteredTitle(text)
@@ -28,13 +34,37 @@ export default function PlaceForm({ createPlaceHandler }: Props) {
   }, [])
 
   const savePlaceHandler = () => {
-    if (enteredTitle && pickedImage && pickedLocation) {
-      createPlaceHandler(enteredTitle, pickedImage, pickedLocation)
+    if (enteredTitle && pickedImage && pickedLocation && address) {
+      createPlaceHandler(enteredTitle, pickedImage, address, pickedLocation)
       setEnteredTitle('')
+      setAddress(null)
       setPickedImage(null)
       setPickedLocation(null)
     }
   }
+
+  const reverseGeocode = async (latitude: number, longitude: number) => {
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`
+      )
+      const data = await response.json()
+      if (data && data.address) {
+        const { road, city, state, postcode, country } = data.address
+        setAddress(`${road}, ${city}, ${state}, ${postcode}, ${country}`)
+      }
+    } catch (error) {
+      console.error('Error in reverse geocoding:', error)
+    }
+  }
+
+  // Trigger reverse geocoding when location changes
+  useEffect(() => {
+    if (pickedLocation) {
+      reverseGeocode(pickedLocation.latitude, pickedLocation.longitude)
+    }
+  }, [pickedLocation])
+
   return (
     <ScrollView className='flex-1 p-6'>
       <View>
--- src/screens/AddPlace.tsx
+++ src/screens/AddPlace.tsx
@@ -5,13 +5,23 @@ import PlaceForm from '~/components/places/PlaceForm'
 import { RootStackParamList } from '~/navigation'
 
 export default function AddPlace({ navigation }: { navigation: NavigationProp<RootStackParamList> }) {
-  const createPlaceHandler = (title: string, imageUri: string, location: { latitude: number; longitude: number }) => {
+  const createPlaceHandler = (
+    title: string,
+    imageUri: string,
+    address: string,
+    location: { latitude: number; longitude: number }
+  ) => {
+    console.log('TITLE: ', title)
+    console.log('IMAGE: ', imageUri)
+    console.log('ADDRESS: ', address)
+    console.log('LOCATION: ', location)
+
     navigation.navigate('AllPlaces', {
       place: {
         id: `${new Date().toString()} - ${Math.random()}`,
         title,
         imageUri,
-        address: 'No Address',
+        address,
         location
       }
     })
